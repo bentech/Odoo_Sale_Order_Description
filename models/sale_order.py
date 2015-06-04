7eb917986236fcbfa3add59735b5bc7a26931976
Ben Swanson <benswanson@rakocontrols.com>
2015-06-04 12:11:04 +0100

    Use Real Parameters

--- models/sale_order.py
+++ models/sale_order.py
@@ -4,17 +4,22 @@ from openerp import models, fields
 class SaleOrder(models.Model):
     _inherit = 'sale.order.line'
 
-    def product_id_change(self, *args, **kwargs):
-        ret = super(SaleOrder, self).product_id_change(*args, **kwargs)
-        cr = args[1]
-        uid = args[2]
-        ids = args[3]
+    def product_id_change_with_wh(self, cr, uid, ids, pricelist, product, qty=0,
+            uom=False, qty_uos=0, uos=False, name='', partner_id=False,
+            lang=False, update_tax=True, date_order=False, packaging=False, fiscal_position=False, flag=False, warehouse_id=False, context=None):
+        product_uom_obj = self.pool.get('product.uom')
         product_obj = self.pool.get('product.product')
-        
-        if not kwargs['flag']:
-            if product_obj.description_sale:
-                ret.value.name = '['+product_obj.default_code+'] '+product_obj.description_sale
-            else:
-                ret.value.name = self.pool.get('product.product').name_get(cr, uid, [product_obj.id], context=kwargs['context_partner'])[0][1]
 
-        return ret
+        res = self.product_id_change(cr, uid, ids, pricelist, product, qty=qty,
+            uom=False, qty_uos=qty_uos, uos=uos, name=name, partner_id=partner_id,
+            lang=lang, update_tax=update_tax, date_order=date_order, packaging=packaging, fiscal_position=fiscal_position, flag=flag, context=context)
+
+        product_obj = product_obj.browse(cr, uid, product, context=context)
+        
+        if not flag:
+            if product_obj.description_sale:
+                res.value.name = '['+product_obj.default_code+'] '+product_obj.description_sale
+            else:
+                res.value.name = self.pool.get('product.product').name_get(cr, uid, [product_obj.id], context=context_partner)[0][1]
+
+        return res
